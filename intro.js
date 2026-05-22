dca127d9cafd23989accf4535f77da496359b5f4
Cyancris <draktar639@gmail.com>
2026-05-22 19:02:35 +0800

    Initial commit

---
+++ intro.js
@@ -0,0 +1,28 @@
+window.addEventListener("scroll", () => {
+
+    const cards = document.querySelectorAll(".content");
+
+    cards.forEach(card => {
+
+        const cardTop = card.getBoundingClientRect().top;
+
+        if(cardTop < window.innerHeight - 100){
+
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+
+        }
+
+    });
+
+});
+
+const cards = document.querySelectorAll(".content");
+
+cards.forEach(card => {
+
+    card.style.opacity = "0";
+    card.style.transform = "translateY(50px)";
+    card.style.transition = "0.6s ease";
+
+});
